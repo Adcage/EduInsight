1a331675463a9efee832700c3865cc1316026f7c
Adcage <2042234720@qq.com>
2025-12-03 12:44:27 +0800

    清理项目无关文件
    - 删除产品和订单相关的API、模型、服务文件
    - 删除旧的model目录(已迁移到schemas)
    - 删除前端示例组件文件
    - 更新相关__init__.py文件的导入

--- backend/app/model/__init__.py
+++
@@ -1,5 +0,0 @@
-from .user_model import *
-from .product_model import *
-from .order_model import *
-from .auth_model import *
-from .common_model import *
--- backend/app/services/__init__.py
+++ backend/app/services/__init__.py
@@ -1,6 +1,4 @@
 from .user_service import UserService
 from .auth_service import AuthService
-from .product_service import ProductService
-from .order_service import OrderService
 
-__all__ = ['UserService', 'AuthService', 'ProductService', 'OrderService']
+__all__ = ['UserService', 'AuthService']
